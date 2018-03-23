8164376b2dfefa25ac7d93e1059db8cea9bdd2e4
Abhishek Shroff <ashroff6@gmail.com>
2018-03-23 16:38:25 -0700

    zshrc

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 # install i3 config
-ln -srT i3-config $HOME/.config/i3/config
+if [[ ! -e "$HOME/.config/i3/config" ]]; then
+    ln -srfT i3-config $HOME/.config/i3/config
+    echo "Installed i3 config"
+else
+    echo "i3 config already installed. Skipping..."
+fi
+
+# install zshrc
+if [[ ! -e "$HOME/.zshrc.custom" ]]; then
+    ln -srT zshrc.custom $HOME/.zshrc.custom
+    echo "source $HOME/.zshrc.custom" >> $HOME/.zshrc
+    echo "Installed zshrc.custom"
+else
+    echo "zshrc.custom already exists. Skipping..."
+fi
