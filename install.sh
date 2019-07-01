ee480ee0bf88b1140ea3d17ad10224cb10367262
Abhishek Shroff <ashroff6@gmail.com>
2019-07-01 19:57:26 +0530

    remove i3 config

--- install.sh
+++ install.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# install i3 config
-mkdir -p $HOME/.config/i3
-if [[ ! -e "$HOME/.config/i3/config" ]]; then
-    ln -srfT i3-config $HOME/.config/i3/config
-    echo "Installed i3 config"
-else
-    echo "i3 config already installed. Skipping..."
-fi
-
-if [[ ! -e "$HOME/.config/i3/config-sway-i3-common" ]]; then
-    ln -srT sway-i3-common-config $HOME/.config/i3/config-sway-i3-common
-    echo "Installed sway-i3-common config"
-else
-    echo "sway-i3-common config already installed. Skipping..."
-fi
-
 # install sway config
 mkdir -p $HOME/.config/sway
 if [[ ! -e "$HOME/.config/sway/config" ]]; then
