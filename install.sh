4466e7b331e4216f14c3deeed95e01158a17e151
Abhishek Shroff <ashroff6@gmail.com>
2019-07-02 11:02:14 +0530

    Update install script to create directory structure

--- install.sh
+++ install.sh
@@ -1,44 +1,17 @@
 #!/bin/bash
 
-# install sway config
-mkdir -p $HOME/.config/sway
-if [[ ! -e "$HOME/.config/sway/config" ]]; then
-    ln -srfT sway-config $HOME/.config/sway/config
-    echo "Installed sway config"
-else
-    echo "sway config already installed. Skipping..."
-fi
+for f in $(find . -type f ! -name install.sh ! -path '*/.git/*'); do
+    mkdir -p $HOME/$(dirname $f)
+    if [[ ! -e "$HOME/$f" ]]; then
+        echo "Installing $(realpath --relative-to=. $f)"
+        ln -srfT $f $HOME/$f
+    else
+        path1=$(readlink -f $HOME/$f)
+	path2=$(readlink -f $f)
+	if [[ $path1 != $path2 ]]; then
+            echo "Unable to install $f as another file exists. Skipping..."
+        fi
+    fi
+done
 
-# install zshrc
-if [[ ! -e "$HOME/.zshrc.custom" ]]; then
-    ln -srT zshrc.custom $HOME/.zshrc.custom
-    echo "source $HOME/.zshrc.custom" >> $HOME/.zshrc
-    echo "Installed zshrc.custom"
-else
-    echo "zshrc.custom already exists. Skipping..."
-fi
 
-# vimrc
-if [[ ! -e "$HOME/.vimrc" ]]; then
-    ln -srT vimrc $HOME/.vimrc
-    echo "Installed vimrc"
-else
-    echo "vimrc already exists. Skipping..."
-fi
-
-# dunstrc
-mkdir -p $HOME/.config/dunst
-if [[ ! -e "$HOME/.config/dunst/dunstrc" ]]; then
-    ln -srT dunstrc $HOME/.config/dunst/dunstrc
-    echo "Installed dunstrc"
-else
-    echo "dunstrc already exists. Skipping..."
-fi
-
-# screnrc
-if [[ ! -e "$HOME/.screenrc" ]]; then
-    ln -srT screenrc $HOME/.screenrc
-    echo "Installed screenrc"
-else
-    echo "screenrc already exists. Skipping..."
-fi
